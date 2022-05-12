a8f19bb05c25659a31807568134fac0c537d275f
davidivad96 <david@strivelabs.io>
2022-05-12 10:55:44 +0200

    Initial commit

---
+++ test/readforme.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Readforme from '../lib/readforme-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/readforme-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Readforme.ReadformeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
