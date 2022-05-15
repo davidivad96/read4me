0104087a5d17771b603da4916b860c474c75de60
davidivad96 <davidivad96@gmail.com>
2022-05-15 22:15:30 +0200

    Simplify overall flow. Use synchronous way instead of asynchronous. Also change StepFunctions state machine type to "EXPRESS"

---
+++ lambdas/synthesizeSpeech/index.js
@@ -0,0 +1,34 @@
+import { PollyClient, SynthesizeSpeechCommand } from "@aws-sdk/client-polly";
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { Upload } from "@aws-sdk/lib-storage";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+const pollyClient = new PollyClient({ region: process.env.CDK_DEPLOY_REGION });
+const s3Client = new S3Client({ region: process.env.CDK_DEPLOY_REGION });
+
+const handler = async (event) => {
+  const { AudioStream } = await pollyClient.send(
+    new SynthesizeSpeechCommand({
+      OutputFormat: "mp3",
+      Text: event.detectDocumentTextResult.Text,
+      VoiceId: event.getVoiceIdResult.VoiceId,
+    })
+  );
+  const Bucket = event.detail.bucket.name;
+  const filename = event.detail.object.key
+    .replace("documents/", "")
+    .toLowerCase()
+    .replace(/[^a-zA-Z\d]/g, "_");
+  const Key = `results/${filename}.mp3`;
+  const upload = new Upload({
+    client: s3Client,
+    params: { Body: AudioStream, Bucket, Key },
+  });
+  await upload.done();
+  const signedUrl = await getSignedUrl(s3Client, new GetObjectCommand({ Bucket, Key }), {
+    expiresIn: 3600,
+  });
+  return signedUrl;
+};
+
+export { handler };
--- lib/readforme-stack.ts
+++ lib/readforme-stack.ts
@@ -7,10 +7,8 @@ import * as targets from "aws-cdk-lib/aws-events-targets";
 import * as sfn from "aws-cdk-lib/aws-stepfunctions";
 import * as tasks from "aws-cdk-lib/aws-stepfunctions-tasks";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as logs from "aws-cdk-lib/aws-logs";
 import { getLambdaFunctionProps, getRandom } from "../utils";
-
-const SQS_QUEUE_ARN = `arn:aws:sqs:${process.env.CDK_DEPLOY_REGION}:${process.env.CDK_DEPLOY_ACCOUNT}:ReadformeJob_*`;
-const SNS_TOPIC_ARN = `arn:aws:sns:${process.env.CDK_DEPLOY_REGION}:${process.env.CDK_DEPLOY_ACCOUNT}:ReadformeJob_*`;
 
 export class ReadformeStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -28,72 +26,30 @@ export class ReadformeStack extends Stack {
 
     /** ------------------ Roles, Policies and Permissions Definition ------------------ */
 
-    const setupTopicAndQueueLambdaPolicy = new iam.ManagedPolicy(
+    const synthesizeSpeechLambdaRolePolicy = new iam.ManagedPolicy(
       this,
-      "SetupTopicAndQueueLambdaPolicy",
+      "SynthesizeSpeechLambdaRolePolicy",
       {
         statements: [
           new iam.PolicyStatement({
             effect: iam.Effect.ALLOW,
-            actions: ["sns:CreateTopic", "sns:Subscribe"],
-            resources: [SNS_TOPIC_ARN],
-          }),
-          new iam.PolicyStatement({
-            effect: iam.Effect.ALLOW,
-            actions: ["sqs:CreateQueue", "sqs:GetQueueAttributes", "sqs:SetQueueAttributes"],
-            resources: [SQS_QUEUE_ARN],
-          }),
-        ],
-      }
-    );
-
-    const setupTopicAndQueueLambdaRole = new iam.Role(this, "SetupTopicAndQueueLambdaRole", {
-      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
-      managedPolicies: [
-        iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole"),
-        setupTopicAndQueueLambdaPolicy,
-      ],
-    });
-
-    const textractSNSPublishPolicy = new iam.ManagedPolicy(this, "TextractSNSPublishPolicy", {
-      statements: [
-        new iam.PolicyStatement({
-          effect: iam.Effect.ALLOW,
-          actions: ["sns:Publish"],
-          resources: [SNS_TOPIC_ARN],
-        }),
-      ],
-    });
-
-    const textractSNSPublishRole = new iam.Role(this, "TextractSNSPublishRole", {
-      assumedBy: new iam.ServicePrincipal("textract.amazonaws.com"),
-      managedPolicies: [textractSNSPublishPolicy],
-    });
-
-    const cleanupTopicAndQueueLambdaPolicy = new iam.ManagedPolicy(
-      this,
-      "CleanupTopicAndQueueLambdaPolicy",
-      {
-        statements: [
-          new iam.PolicyStatement({
-            effect: iam.Effect.ALLOW,
-            actions: ["sns:Unsubscribe"],
+            actions: ["polly:SynthesizeSpeech"],
             resources: ["*"],
           }),
           new iam.PolicyStatement({
             effect: iam.Effect.ALLOW,
-            actions: ["sns:DeleteTopic", "sqs:DeleteQueue"],
-            resources: [SNS_TOPIC_ARN, SQS_QUEUE_ARN],
+            actions: ["s3:GetObject", "s3:PutObject"],
+            resources: [`${s3Bucket.bucketArn}/results/*`],
           }),
         ],
       }
     );
 
-    const cleanupTopicAndQueueLambdaRole = new iam.Role(this, "CleanupTopicAndQueueLambdaRole", {
+    const synthesizeSpeechLambdaRole = new iam.Role(this, "SynthesizeSpeechLambdaRole", {
       assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
       managedPolicies: [
         iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole"),
-        cleanupTopicAndQueueLambdaPolicy,
+        synthesizeSpeechLambdaRolePolicy,
       ],
     });
 
@@ -103,21 +59,6 @@ export class ReadformeStack extends Stack {
           effect: iam.Effect.ALLOW,
           actions: ["s3:GetObject"],
           resources: [`${s3Bucket.bucketArn}/documents/*`],
-        }),
-        new iam.PolicyStatement({
-          effect: iam.Effect.ALLOW,
-          actions: ["s3:PutObject"],
-          resources: [`${s3Bucket.bucketArn}/results/*`],
-        }),
-        new iam.PolicyStatement({
-          effect: iam.Effect.ALLOW,
-          actions: ["sns:Publish"],
-          resources: [SNS_TOPIC_ARN],
-        }),
-        new iam.PolicyStatement({
-          effect: iam.Effect.ALLOW,
-          actions: ["sqs:ReceiveMessage", "sqs:DeleteMessage"],
-          resources: [SQS_QUEUE_ARN],
         }),
       ],
     });
@@ -129,22 +70,10 @@ export class ReadformeStack extends Stack {
 
     /** ------------------ Lambda Handlers Definition ------------------ */
 
-    const checkDocumentLambda = new lambda.Function(
+    const parseTextLambda = new lambda.Function(
       this,
-      "CheckDocument",
-      getLambdaFunctionProps("checkDocument")
-    );
-
-    const setupTopicAndQueueLambda = new lambda.Function(
-      this,
-      "SetupTopicAndQueue",
-      getLambdaFunctionProps("setupTopicAndQueue", setupTopicAndQueueLambdaRole)
-    );
-
-    const transformBlocksToTextLambda = new lambda.Function(
-      this,
-      "TransformBlocksToText",
-      getLambdaFunctionProps("transformBlocksToText")
+      "ParseTextLambda",
+      getLambdaFunctionProps("parseText")
     );
 
     const getVoiceIdLambda = new lambda.Function(
@@ -153,127 +82,42 @@ export class ReadformeStack extends Stack {
       getLambdaFunctionProps("getVoiceId")
     );
 
-    const cleanupTopicAndQueueLambda = new lambda.Function(
+    const synthesizeSpeechLambda = new lambda.Function(
       this,
-      "CleanupTopicAndQueue",
-      getLambdaFunctionProps("cleanupTopicAndQueue", cleanupTopicAndQueueLambdaRole)
+      "SynthesizeSpeech",
+      getLambdaFunctionProps("synthesizeSpeech", synthesizeSpeechLambdaRole)
     );
 
-    /** ------------------ Tasks and States Definition ------------------ */
+    /** ------------------ Tasks Definition ------------------ */
 
-    const checkDocumentTask = new tasks.LambdaInvoke(this, "Check Document", {
-      lambdaFunction: checkDocumentLambda,
+    const detectDocumentTextTask = new tasks.CallAwsService(this, "DetectDocumentTextTask", {
+      service: "textract",
+      action: "detectDocumentText",
+      parameters: {
+        Document: {
+          S3Object: {
+            Bucket: sfn.JsonPath.stringAt("$.bucket.name"),
+            Name: sfn.JsonPath.stringAt("$.object.key"),
+          },
+        },
+      },
       inputPath: "$.detail",
-      outputPath: "$.Payload",
+      resultPath: "$.detectDocumentTextResult",
+      iamResources: ["*"],
     });
 
-    const documentTooLargeFailTask = new sfn.Fail(this, "Fail: Document Too Large", {
-      error: "DocumentTooLarge",
-      cause: "Size limit is 5MB!",
-    });
-
-    const unsupportedDocumentFailTask = new sfn.Fail(this, "Fail: Unsupported Document", {
-      error: "UnsupportedDocument",
-      cause: "Allowed documents: PDF, PNG, JPG, JPEG and TIFF",
-    });
-
-    const setupTopicAndQueueTask = new tasks.LambdaInvoke(this, "Setup Topic And Queue", {
-      lambdaFunction: setupTopicAndQueueLambda,
-      outputPath: "$.Payload",
-    });
-
-    const startDocumentTextDetectionTask = new tasks.CallAwsService(
-      this,
-      "Start Document Text Detection",
-      {
-        service: "textract",
-        action: "startDocumentTextDetection",
-        parameters: {
-          DocumentLocation: {
-            S3Object: {
-              Bucket: sfn.JsonPath.stringAt("$.bucketName"),
-              Name: sfn.JsonPath.stringAt("$.objectKey"),
-            },
-          },
-          NotificationChannel: {
-            RoleArn: textractSNSPublishRole.roleArn,
-            SnsTopicArn: sfn.JsonPath.stringAt("$.topicArn"),
-          },
-        },
-        resultPath: "$.startDocumentTextDetectionResult",
-        iamResources: ["*"],
-      }
-    );
-
-    const receiveTextractJobMessageTask = new tasks.CallAwsService(
-      this,
-      "Receive Textract Job Message",
-      {
-        service: "sqs",
-        action: "receiveMessage",
-        parameters: { QueueUrl: sfn.JsonPath.stringAt("$.queueUrl") },
-        resultPath: "$.receiveTextractJobMessageResult",
-        iamResources: [SQS_QUEUE_ARN],
-      }
-    );
-
-    const checkTextractJobMessageReceivedChoice = new sfn.Choice(
-      this,
-      "Choice: Check If Received Textract Job Message"
-    );
-
-    const deleteTextractJobMessageTask = new tasks.CallAwsService(
-      this,
-      "Delete Textract Job Message",
-      {
-        service: "sqs",
-        action: "deleteMessage",
-        parameters: {
-          QueueUrl: sfn.JsonPath.stringAt("$.queueUrl"),
-          ReceiptHandle: sfn.JsonPath.stringAt(
-            "$.receiveTextractJobMessageResult.Messages[0].ReceiptHandle"
-          ),
-        },
-        resultPath: sfn.JsonPath.DISCARD,
-        iamResources: [SQS_QUEUE_ARN],
-      }
-    );
-
-    const getDocumentTextDetectionTask = new tasks.CallAwsService(
-      this,
-      "Get Document Text Detection",
-      {
-        service: "textract",
-        action: "getDocumentTextDetection",
-        parameters: { JobId: sfn.JsonPath.stringAt("$.JobId") },
-        inputPath: "$.startDocumentTextDetectionResult",
-        resultPath: "$.getDocumentTextDetectionResult",
-        iamResources: ["*"],
-      }
-    );
-
-    const transformBlocksToText = new tasks.LambdaInvoke(this, "Transform Blocks To Text", {
-      lambdaFunction: transformBlocksToTextLambda,
-      inputPath: "$.getDocumentTextDetectionResult",
+    const parseTextTask = new tasks.LambdaInvoke(this, "Parse Text", {
+      lambdaFunction: parseTextLambda,
+      inputPath: "$.detectDocumentTextResult",
       resultSelector: { "Text.$": "$.Payload" },
-      resultPath: "$.getDocumentTextDetectionResult",
-    });
-
-    const cleanupTopicAndQueueTask1 = new tasks.LambdaInvoke(this, "Cleanup Topic And Queue 1", {
-      lambdaFunction: cleanupTopicAndQueueLambda,
-      resultPath: sfn.JsonPath.DISCARD,
-    });
-
-    const noTextFoundFailTask = new sfn.Fail(this, "Fail: No Text Found", {
-      error: "NoTextFound",
-      cause: "No text was found in the document!",
+      resultPath: "$.detectDocumentTextResult",
     });
 
     const detectDominantLanguageTask = new tasks.CallAwsService(this, "Detect Dominant Language", {
       service: "comprehend",
       action: "detectDominantLanguage",
       parameters: { Text: sfn.JsonPath.stringAt("$.Text") },
-      inputPath: "$.getDocumentTextDetectionResult",
+      inputPath: "$.detectDocumentTextResult",
       resultSelector: { "LanguageCode.$": "$.Languages[0].LanguageCode" },
       resultPath: "$.detectDominantLanguageResult",
       iamResources: ["*"],
@@ -286,119 +130,32 @@ export class ReadformeStack extends Stack {
       resultPath: "$.getVoiceIdResult",
     });
 
-    const startSpeechSynthesisTask = new tasks.CallAwsService(this, "Start Speech Synthesis", {
-      service: "polly",
-      action: "startSpeechSynthesisTask",
-      parameters: {
-        OutputFormat: "mp3",
-        OutputS3BucketName: sfn.JsonPath.stringAt("$.bucketName"),
-        OutputS3KeyPrefix: sfn.JsonPath.format("results/{}", sfn.JsonPath.stringAt("$.filename")),
-        Text: sfn.JsonPath.stringAt("$.getDocumentTextDetectionResult.Text"),
-        VoiceId: sfn.JsonPath.stringAt("$.getVoiceIdResult.VoiceId"),
-        SnsTopicArn: sfn.JsonPath.stringAt("$.topicArn"),
-      },
-      resultSelector: { "TaskId.$": "$.SynthesisTask.TaskId" },
-      resultPath: "$.startSpeechSynthesisTaskResult",
-      iamResources: ["*"],
-    });
-
-    const receivePollyTaskMessageTask = new tasks.CallAwsService(
-      this,
-      "Receive Polly Task Message",
-      {
-        service: "sqs",
-        action: "receiveMessage",
-        parameters: { QueueUrl: sfn.JsonPath.stringAt("$.queueUrl") },
-        resultPath: "$.receivePollyTaskMessageResult",
-        iamResources: [SQS_QUEUE_ARN],
-      }
-    );
-
-    const checkPollyTaskMessageReceivedChoice = new sfn.Choice(
-      this,
-      "Choice: Check If Received Polly Task Message"
-    );
-
-    const deletePollyTaskMessageTask = new tasks.CallAwsService(this, "Delete Polly Task Message", {
-      service: "sqs",
-      action: "deleteMessage",
-      parameters: {
-        QueueUrl: sfn.JsonPath.stringAt("$.queueUrl"),
-        ReceiptHandle: sfn.JsonPath.stringAt(
-          "$.receivePollyTaskMessageResult.Messages[0].ReceiptHandle"
-        ),
-      },
-      resultPath: sfn.JsonPath.DISCARD,
-      iamResources: [SQS_QUEUE_ARN],
-    });
-
-    const getSpeechSynthesisTask = new tasks.CallAwsService(this, "Get Speech Synthesis", {
-      service: "polly",
-      action: "getSpeechSynthesisTask",
-      parameters: { TaskId: sfn.JsonPath.stringAt("$.TaskId") },
-      inputPath: "$.startSpeechSynthesisTaskResult",
-      resultPath: "$.getSpeechSynthesisTaskResult",
-      iamResources: ["*"],
-    });
-
-    const cleanupTopicAndQueueTask2 = new tasks.LambdaInvoke(this, "Cleanup Topic And Queue 2", {
-      lambdaFunction: cleanupTopicAndQueueLambda,
-      resultPath: sfn.JsonPath.DISCARD,
+    const synthesizeSpeechTask = new tasks.LambdaInvoke(this, "Synthesize Speech", {
+      lambdaFunction: synthesizeSpeechLambda,
+      resultSelector: { "SignedUrl.$": "$.Payload" },
+      resultPath: "$.synthesizeSpeechResult",
     });
 
     /** ------------------ Step Function Definition ------------------ */
 
-    const definition = checkDocumentTask
-      .addCatch(documentTooLargeFailTask, { errors: ["DocumentTooLarge"], resultPath: "$.error" })
-      .addCatch(unsupportedDocumentFailTask, {
-        errors: ["UnsupportedDocument"],
-        resultPath: "$.error",
-      })
-      .next(setupTopicAndQueueTask)
-      .next(startDocumentTextDetectionTask)
-      .next(receiveTextractJobMessageTask)
-      .next(
-        checkTextractJobMessageReceivedChoice
-          .when(
-            sfn.Condition.isNotPresent("$.receiveTextractJobMessageResult.Messages"),
-            receiveTextractJobMessageTask
-          )
-          .otherwise(
-            deleteTextractJobMessageTask.next(
-              getDocumentTextDetectionTask.next(
-                transformBlocksToText
-                  .addCatch(cleanupTopicAndQueueTask1.next(noTextFoundFailTask), {
-                    errors: ["NoTextFound"],
-                    resultPath: "$.error",
-                  })
-                  .next(detectDominantLanguageTask)
-                  .next(getVoiceIdTask)
-                  .next(startSpeechSynthesisTask)
-                  .next(receivePollyTaskMessageTask)
-                  .next(
-                    checkPollyTaskMessageReceivedChoice
-                      .when(
-                        sfn.Condition.isNotPresent("$.receivePollyTaskMessageResult.Messages"),
-                        receivePollyTaskMessageTask
-                      )
-                      .otherwise(
-                        deletePollyTaskMessageTask
-                          .next(getSpeechSynthesisTask)
-                          .next(cleanupTopicAndQueueTask2)
-                      )
-                  )
-              )
-            )
-          )
-      );
+    const definition = detectDocumentTextTask
+      .next(parseTextTask)
+      .next(detectDominantLanguageTask)
+      .next(getVoiceIdTask)
+      .next(synthesizeSpeechTask);
 
     const readformeStateMachine = new sfn.StateMachine(this, "ReadForMe", {
       definition,
       timeout: Duration.minutes(5),
       stateMachineName: "ReadForMe",
-      stateMachineType: sfn.StateMachineType.STANDARD,
+      stateMachineType: sfn.StateMachineType.EXPRESS,
       tracingEnabled: true,
       role: readformeStateMachineRole,
+      logs: {
+        destination: new logs.LogGroup(this, "ReadForMeLogGroup"),
+        includeExecutionData: true,
+        level: sfn.LogLevel.ALL,
+      },
     });
 
     /** ------------------ EventBridge Rule Definition ------------------ */
