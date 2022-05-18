4a1c62f6b253f12185c8b9907f6156a29d42b816
davidivad96 <davidivad96@gmail.com>
2022-05-18 22:12:16 +0200

    Implement Spinner component

---
+++ frontend/src/components/Spinner.tsx
@@ -0,0 +1,14 @@
+import "./Spinner.css";
+
+const Spinner = () => (
+  <div className="sk-chase">
+    <div className="sk-chase-dot"></div>
+    <div className="sk-chase-dot"></div>
+    <div className="sk-chase-dot"></div>
+    <div className="sk-chase-dot"></div>
+    <div className="sk-chase-dot"></div>
+    <div className="sk-chase-dot"></div>
+  </div>
+);
+
+export default Spinner;
