c2906bebba1c1902bd419a5313446b00062d387c
davidivad96 <david@strivelabs.io>
2022-05-19 22:00:21 +0200

    Remove legacy code

--- lambdas/parseText/index.js
+++ lambdas/parseText/index.js
@@ -1,18 +1,8 @@
-class NoTextFound extends Error {
-  constructor(message) {
-    super(message);
-    this.name = this.constructor.name;
-  }
-}
-
 const handler = async (event) => {
   const text = event.Blocks.reduce(
     (acc, curr) => (curr.BlockType === "LINE" && curr.Text ? `${acc.concat(curr.Text)} ` : acc),
     ""
   );
-  if (text.length === 0) {
-    throw new NoTextFound("No text was found in the document");
-  }
   return text;
 };
 
